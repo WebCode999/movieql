2cbea1c737a9fb2dc7f3dc55c8351d760c0a7553
TopProg <top.tech.prog@gmail.com>
2019-10-02 12:08:27 +0800

    createing query and resolver

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {
     GraphQLSever
 } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
 const sever = new GraphQLSever({
-
+    typeDefs: "graphql/schema.graphql",
+    resolvers
 });
 
 sever.start(() => console.log("Graphql Server Running"));
